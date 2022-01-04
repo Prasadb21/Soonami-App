40d656609520026bbedc67b2d0bf8b86a560ed9c
Prasad Bhalkikar <prasadbhalkikar06@gmail.com>
2022-01-04 13:56:30 +0530

    final App Commit

---
+++ app/src/main/java/com/example/soonami/Event.java
@@ -0,0 +1,21 @@
+package com.example.soonami;
+
+public class Event {
+
+    /** Title of the earthquake event */
+    public final String title;
+
+    /** Time that the earthquake happened (in milliseconds) */
+    public final long time;
+
+    /** Whether or not a tsunami alert was issued (1 if it was issued, 0 if no alert was issued) */
+    public final int tsunamiAlert;
+
+
+    public Event(String eventTitle, long eventTime, int eventTsunamiAlert) {
+        title = eventTitle;
+        time = eventTime;
+        tsunamiAlert = eventTsunamiAlert;
+    }
+}
+
